fb66e519c7a34324ef0980b4d30d88a53964f0a0
Jean-Lionel <nijeanlionel@gmail.com>
2022-11-07 05:50:43 +0200

    login form

--- src/views/BaseLogin.vue
+++ src/views/BaseLogin.vue
@@ -1,22 +1,26 @@
 <template>
     <div class="base-login">
-        
+        <nav-bar/>
+        <Welcome/>
+        <login-form/>
         <Footer/>
     </div>
 </template>
 
 <script>
 import Footer from '../components/Login/Footer.vue';
+import LoginForm from '../components/Login/LoginForm.vue';
+import NavBar from '../components/Login/NavBar.vue';
+import Welcome from '../components/Login/Welcome.vue';
 
 export default{
-    components: { Footer }
+    components: { Footer, NavBar, Welcome, LoginForm }
 }
 </script>
 
 <style scoped>
 .base-login{
-    height: 100vh;
-    width: 100vw;
+    
 }
 </style>
 
