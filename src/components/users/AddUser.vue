3247d2a244f90730654c4fc519c47349097755f9
Jean-Lionel <nijeanlionel@gmail.com>
2022-11-16 06:39:18 +0200

    adding add user form

--- src/components/users/AddUser.vue
+++ src/components/users/AddUser.vue
@@ -1,7 +1,45 @@
 <template>
     <div>
         <modal-component :modalActive="modalActive" @close="close">
-            <h1>Je suis Un Millionnaire</h1>
+            <div>
+                <form action="" @submit.prevent="saveUser">
+                    <div>
+                        <label for=""> name </label>
+                        <input type="text" v-model="form.name" />
+                    </div>
+                    
+                    <div>
+                        <label for=""> first_name </label>
+                        <input type="text" v-model="form.first_name" />
+                    </div>
+                    
+                    <div>
+                        <label for=""> last_name </label>
+                        <input type="text" v-model="form.last_name" />
+                    </div>
+                    
+                    <div>
+                        <label for=""> telephone </label>
+                        <input type="text" v-model="form.telephone" />
+                    </div>
+                    <div>
+                        <label for=""> description </label>
+                        <input type="text" v-model="form.description" />
+                    </div>
+                    <div>
+                        <label for=""> email </label>
+                        <input type="text" v-model="form.email" />
+                    </div>
+                    
+                    <div>
+                        <label for=""> password </label>
+                        <input type="text" v-model="form.password" />
+                    </div>
+                    <div>
+                    <button type="submit">Enregitrer</button>
+                    </div>
+                </form>
+            </div>
         </modal-component>
     </div>
 </template>
@@ -13,12 +51,25 @@ export default {
     data() {
         return{
             modalActive : true,
+            form: {
+                name : "",
+                first_name : "",
+                last_name : "",
+                telephone : "",
+                description : "",
+                is_active : "",
+                email : "",
+                password : "",
+            }
         }
     },
     methods: {
         close() {
             this.modalActive = false
             this.$emit('close')
+        },
+        saveUser() {
+            
         }
     }
     
