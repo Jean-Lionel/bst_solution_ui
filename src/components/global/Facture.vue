461b245bcd1fb6b5bd3f03d166cd6f2db3e6e4fa
Jean-Lionel <nijeanlionel@gmail.com>
2022-12-06 06:04:48 +0200

    add information of client

--- src/components/global/Facture.vue
+++ src/components/global/Facture.vue
@@ -1,88 +1,95 @@
 <template>
-    <div>
-        <button class="print" @click="printFacture"> <i class="fa fa-print"></i> Imprimer</button>
-        <div id="invoice">
-            <div>
-            </div>
-            <header class="invoice_header">
-                <div>
-                    <h1>Facture</h1>
-                </div>
-                <div class="text-right">
-                    <div>Serial No. <b> {{ order.id }}</b></div>
-                    <div>Invoice date: <b>{{ order.created_at }}</b></div>
-                </div>
-            </header>
-            <section class="invoice_section">
-                <div>
-                    <h4>Seller</h4>
-                    <div class="line"></div>
-                    <div>
-                        <div>Towne, Smith and Ebert</div>
-                        <div>Address: 89982 Pfeffer Falls Damianstad, CO</div>
-                        <div>66972-8160</div>
-                        <div>Code: 41-1985581</div>
-                        <div>VAT code: 123456789</div>
-                        <div>Phone: 760-355-3930</div>
-                        <div>SWIFT: BANK101</div>
-                        
-                    </div>
-                </div>
-                <div>
-                    <h4>Buyer</h4>
-                    <div class="line"></div>
-                    <div>
-                        <div>John Doe</div>
-                        <div>Email: test@example.com</div>
-                    </div>
-                </div>
-            </section>
-            <section>
-                <div>
-                    <table>
-                        <thead>
-                            <tr class="line">
-                                <th class="text-left">Description</th>
-                                <th>Qty</th>
-                                <th>Price</th>
-                                <th>Sub total</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr v-for="(product, i ) in products" :key="i">
-                                <td class="text-left">{{ product.name }}</td>
-                                <td class="line">{{ product.quantity }}</td>
-                                <td class="line">{{ product.price }} </td>
-                                <th class="line">{{ product.prix_total }} </th>
-                            </tr>
-                            <tr>
-                                <td colspan="3">Prix Hors TVA </td>
-                                <th class="line">{{ order.amount }}</th>
-                            </tr>
-                            <tr>
-                                <td colspan="3">Tax </td>
-                                <th class="line">{{ order.tax }}</th>
-                            </tr>
-                            <tr>
-                                <td colspan="3">TOTAL</td>
-                                <th class="line">{{ order.amount_tax }}</th>
-                            </tr>
-                            
-                            
-                        </tbody>
-                    </table>
-                </div>
-            </section>
-            <footer>
-                <p> Nininahazwe Jean Lionel</p>
-                <!-- <p>Tél <i>+257 79 614 036 / 61 444 953</i></p>
-                    <p> Adresse <i>Bujumbura-Burundi</i></p> -->
-                    <p>&copy; Pour usage </p>
-                    
-                </footer>
-            </div>
+<div>
+    <button class="print" @click="printFacture"> <i class="fa fa-print"></i> Imprimer</button>
+    <div id="invoice">
+    <div id="invoice_element">
+        <div>
         </div>
-    </template>
+        <header class="invoice_header">
+            <div>
+                <h1>Facture</h1>
+            </div>
+            <div class="text-right">
+                <div>Serial No. <b> {{ order.id }}</b></div>
+                <div>Invoice date: <b>{{ order.created_at }}</b></div>
+            </div>
+        </header>
+        <section class="invoice_section">
+            <div>
+                <h4>Seller</h4>
+                <div class="line"></div>
+                <div class="tex-left">
+                    <div>Towne, Smith and Ebert</div>
+                    <div>Address: 89982 Pfeffer Falls Damianstad, CO</div>
+                    <div>66972-8160</div>
+                    <div>Code: 41-1985581</div>
+                    <div>VAT code: 123456789</div>
+                    <div>Phone: 760-355-3930</div>
+                    <div>SWIFT: BANK101</div>
+    
+                </div>
+    
+                <div class="paiement-method">
+                    <h4>Methode de Paiement</h4>
+                    <b>CASH</b>
+                </div>
+            </div>
+            <div>
+                <h4>Buyer</h4>
+                <div class="line"></div>
+                <div class="text-right">
+                    <div>John Doe</div>
+                    <div>Email: test@example.com</div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div>
+                <table>
+                    <thead>
+                        <tr class="line">
+                            <th class="text-left">Description</th>
+                            <th>Qty</th>
+                            <th>Price</th>
+                            <th>Sub total</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="(product, i ) in products" :key="i">
+                            <td class="text-left">{{ product.name }}</td>
+                            <td class="line">{{ product.quantity }}</td>
+                            <td class="line">{{ product.price }} </td>
+                            <th class="line">{{ product.prix_total }} </th>
+                        </tr>
+                        <tr>
+                            <td colspan="3">Prix Hors TVA </td>
+                            <th class="line">{{ order.amount }}</th>
+                        </tr>
+                        <tr>
+                            <td colspan="3">Tax </td>
+                            <th class="line">{{ order.tax }}</th>
+                        </tr>
+                        <tr>
+                            <td colspan="3">TOTAL</td>
+                            <th class="line">{{ order.amount_tax }}</th>
+                        </tr>
+    
+    
+                    </tbody>
+                </table>
+            </div>
+        </section>
+        <footer>
+            <p> Nininahazwe Jean Lionel</p>
+            <!-- <p>Tél <i>+257 79 614 036 / 61 444 953</i></p>
+                    <p> Adresse <i>Bujumbura-Burundi</i></p> -->
+            <p>&copy; Pour usage </p>
+    
+        </footer>
+    </div>
+    </div>
+    </div>
+</template>
     
 <script>
     export default {
@@ -110,9 +117,16 @@
 .print{
     padding: 6px 6px;
 }
-.invoice{
-    margin-top: 400px;
-    font-family: "Open Sans", sans-serif;
+#invoice{
+    margin-top: 10px;
+}
+
+@media print {
+    #invoice_element {
+            font-family: "Open Sans", sans-serif;
+            padding: 10px 40px;
+            height: auto;
+        }
 }
 
 header {
@@ -144,6 +158,7 @@ table {
 }
 
 .line {
+    margin: 10px 0px;
     border-bottom: 2px solid #ccc;
 }
 
@@ -164,5 +179,8 @@ footer {
 footer p {
     font-family: 'Tangerine', serif;
     font-size: 36px;
+}
+.paiement-method{
+    padding: 10px 0 0 0;
 }
 </style>
