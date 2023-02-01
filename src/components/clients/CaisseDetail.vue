2b8476b117c3b697baf7dc30f1b00418fa932f63
Jean-Lionel <nijeanlionel@gmail.com>
2023-02-01 06:55:34 +0200

    detail de caisse

--- src/components/clients/CaisseDetail.vue
+++ src/components/clients/CaisseDetail.vue
@@ -1,12 +1,53 @@
 <template>
-    <div>
-        <h2>Caisse détail</h2>
+    <div >
+        <h2>Historique des opération du caisse </h2>
+        <div class="table_containner">
+        <table>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Utilisateur</th>
+                    <th>Opération</th>
+                    <th>Montant</th>
+                    <th>Date</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(operation, index) in caisses_detail">
+                <td>{{ ++index }}</td>
+                <td> {{ operation.user.name }}</td>
+                <td> {{ operation.type_operations }}</td>
+                <td> {{ operation.montant }}</td>
+                <td> {{ operation.created_at }}</td>
+                </tr>
+            </tbody>
+        </table>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            caisses_detail : []
+        }
+    },
+    mounted() {
+        this.get();
+     },
+    methods: {
+        get() {
+            this.getData(`caisse_operations/${this.$route.params.id}`)
+                .then(resp => {
+                    this.caisses_detail = resp.data
+                })
+                ;
+        }
+    },
+    computed: {
 
+    }
 }
 </script>
 
